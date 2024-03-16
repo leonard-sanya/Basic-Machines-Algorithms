da84f12123dc7402ee2f2d58f904c569ff64003a
leonard sanya <lsanya@aimsammi.org>
2024-03-16 13:01:17 +0000

    created a main.py file

---
+++ Main.py
@@ -0,0 +1,41 @@
+from Linear_regression import LinearRegression
+from Logistic_regression import LogisticRegression 
+
+#linear regression
+#np.random.seed(10)
+x = np.linspace(0,1, 10)
+y = xtrain + np.random.normal(0, 0.1, (10,))
+
+x = x.reshape(-1, 1)
+y = y.reshape(-1, 1)
+
+
+#logistic data
+X, y = make_classification(n_features=2, n_redundant=0,
+                           n_informative=2, random_state=1,
+                           n_clusters_per_class=1)
+def train_test_split(X,y):
+  '''
+  this function takes as input the sample X and the corresponding features y
+  and output the training and test set
+  '''
+  #np.random.seed(0) 
+  train_size = 0.8
+  n = int(len(X)*train_size)
+  indices = np.arange(len(X))
+  np.random.shuffle(indices)
+  train_idx = indices[: n]
+  test_idx = indices[n:]
+  X_train, y_train = X[train_idx], y[train_idx]
+  X_test, y_test = X[test_idx], y[test_idx]
+
+  return X_train, y_train, X_test, y_test
+
+X_train, y_train, X_test, y_test = train_test_split(X,y)
+
+linear = LinearRegression()
+logistic = LogisticRegression()
+
+def main():
+    if __name__ == "__main__":
+    main()
